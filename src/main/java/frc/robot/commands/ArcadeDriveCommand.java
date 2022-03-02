fc9493f0138cd4c1966483dc958043a3989b96f1
sabon <87005326+illusi0nnn@users.noreply.github.com>
2022-03-02 13:38:10 +0200

    adding comments to the Arcade Drive Command
    adding comments to the new changes that were added to the Arcade Drive Command

--- src/main/java/frc/robot/commands/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -6,17 +6,17 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ArcadeDriveCommand extends CommandBase{
     @Override
     public void initialize() {
-        addRequirements(m_robotContainer.getDriverSubsystem());
-        m_robotContainer.getDriverSubsystem().feed();
+        addRequirements(m_robotContainer.getDriverSubsystem());//adding the getter for the DriverSubsystem
+        m_robotContainer.getDriverSubsystem().feed();//giving more data for the watch dog
     }
     @Override
     public void execute() {
-        m_robotContainer.getDriverSubsystem().feed();
-        m_robotContainer.getDriverSubsystem().ArcadeDrive((m_robotContainer.getStick().getY()/1.2)*0.9, (m_robotContainer.getStick().getX()/1.2)*0.9);
+        m_robotContainer.getDriverSubsystem().feed();//giving data for the watch dog
+        /*calling the arcadeDrive function*/m_robotContainer.getDriverSubsystem().ArcadeDrive(/*setting limitations to the max input for each access of the joystick*/(m_robotContainer.getStick().getY()/1.2)*0.9, (m_robotContainer.getStick().getX()/1.2)*0.9);
     }
     @Override
     public void end(boolean interrupted) {
-        m_robotContainer.getDriverSubsystem().feed();
-        m_robotContainer.getDriverSubsystem().StopDrive();
+        m_robotContainer.getDriverSubsystem().feed();//giving more data for the watch dog
+        m_robotContainer.getDriverSubsystem().StopDrive();//calling the stop function to stop the movement of the robot.
     }
 }
