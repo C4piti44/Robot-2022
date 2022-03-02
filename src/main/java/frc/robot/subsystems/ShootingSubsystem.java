942d022e8635606b6d81f2640b617f6d12fe9c0b
sabon <87005326+illusi0nnn@users.noreply.github.com>
2022-03-02 13:24:40 +0200

    adding comments to shooting subsytem
    adding comments to the new changes that were added to the Shooting subsystem.

--- src/main/java/frc/robot/subsystems/ShootingSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -4,18 +4,24 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class ShootingSubsystem extends SubsystemBase{
-    private WPI_TalonSRX shootingMotor;
+    private WPI_TalonSRX shootingMotor;//creatinga talon motor object
     
     public ShootingSubsystem(){
-        this.shootingMotor = new WPI_TalonSRX(1);
+        this.shootingMotor = new WPI_TalonSRX(1);//giving said object a value.
     }
     public void shoot(double power){
-        this.shootingMotor.set(ControlMode.PercentOutput, power);
+        this.shootingMotor.set(ControlMode.PercentOutput, power);//making the motor spin at percentage giving to it in the ShootingCommand
     }
-    // public void yshoot(Joystick stick){
+    /* public void yshoot(Joystick stick){//this function was for a testing the power that will be used for the shooting, bye using the values of the Y access in the joystick.  
+                0-----0    y
+   Joystick  -> | (O) |   / \
+                0-----0    |
+                          \ /
+                           -y
+                
     //         this.shootingMotor.set(ControlMode.PercentOutput, stick.getY()*0.9);
-    // } 
+     }*/ 
     public void stop(){
-        this.shootingMotor.set(ControlMode.PercentOutput,0);
+        this.shootingMotor.set(ControlMode.PercentOutput,0);//making the motor stop spinning.
     }
 }
